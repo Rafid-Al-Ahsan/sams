1eaeb180b0a64f415c04ed05f41484cf83495390
Rafid-Al-Ahsan <rafidahsan97@gmail.com>
2021-08-08 11:49:43 +0600

    Add files via upload

---
+++ sams.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 08, 2021 at 07:49 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sams`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `userid` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `usertype` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`userid`, `password`, `usertype`) VALUES
+('001', '1234', 'student'),
+('2001', '5678', 'teacher'),
+('admin', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `roll_no` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `age` varchar(30) NOT NULL,
+  `gender` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`roll_no`, `name`, `age`, `gender`, `email`) VALUES
+('001', 'Sakimuzzaman', '16', 'Male', 'sk@gmail.com'),
+('002', 'Misbahur', '15', 'Male', 'misbahur@gmail.com'),
+('003', 'Rafid', '16', 'Male', 'rafid@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `si` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`si`, `name`) VALUES
+('01', 'English'),
+('02', 'Bangla'),
+('03', 'Maths'),
+('04', 'Science'),
+('05', 'History'),
+('06', 'Geography'),
+('07', 'Music'),
+('08', 'Arts'),
+('09', 'Sports');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `id` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `age` varchar(30) NOT NULL,
+  `gender` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`id`, `name`, `age`, `gender`, `email`) VALUES
+('001', 'Kamal ', '40', 'Male', 'kamal@gmail.com'),
+('002', 'Akter', '37', 'Male', 'akter@yahooo.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`roll_no`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`si`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
